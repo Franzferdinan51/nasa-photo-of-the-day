acd1f117bfe9ef46daa22e4130a85abe0401ebe1
Franzferdinan51 <franzferdinan51@gmail.com>
2019-10-24 13:48:12 -0400

    trying to finish

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./App.css";
+import CardHolder from "./Component/CardHolder";
 
 function App() {
   return (
     <div className="App">
+    <CardHolder/>
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
